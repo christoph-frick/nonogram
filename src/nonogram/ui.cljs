d030ac48c59eac9aa7d3100111f49d98a7348d07
Christoph Frick <cf@ofnir.net>
2017-05-23 18:48:55 +0200

    Have some styling and some simple setup handling

--- src/nonogram/ui.cljs
+++ src/nonogram/ui.cljs
@@ -1,15 +1,35 @@
 (ns nonogram.ui
   (:require [rum.core :as rum]
+            [rum.mdl :as mdl]
             [clojure.string :as str]
             [nonogram.game :as game]
             [nonogram.tools :as t]))
 
+; TODO move all the defn over to game ns
 (defonce STATE
   (atom
    (-> (game/new-board-setup 5 5 0.5)
        #_(game/new-board-setup (+ 3 (rand-int 17)) (+ 3 (rand-int 17)) (rand))
        (game/new-board)
        (game/new-game-board))))
+
+(defn new-game!
+  []
+  (println "QWER")
+  (swap! STATE (comp game/new-game-board game/new-board)))
+
+(defn restart-game!
+  []
+  (println "ASDF")
+  (swap! STATE game/new-game-board))
+
+(defn value
+  [e]
+  (-> e .-target .-value))
+
+(defn update-setup!
+  [key value]
+  (swap! STATE assoc key value))
 
 (defn simple-key [args]
   (str/join "-" (map str args)))
@@ -47,13 +67,12 @@
    (for [[ci c] (map vector (range) state)]
      (cell c row-index ci))])
 
-(rum/defc game
-  []
-  (let [{:board/keys [col-hints row-hints] :game-board/keys [rows] :as board} @STATE
-        max-col-hints (t/max-count col-hints)
+(rum/defc board
+  [{:board/keys [col-hints row-hints] :game-board/keys [rows] :as state}]
+  (let [max-col-hints (t/max-count col-hints)
         max-row-hints (t/max-count row-hints)]
     [:div
-     (when (game/won? board)
+     (when (game/won? state)
        [:h1 "You won!"])
      [:.game
       [:.col-hint-container
@@ -66,3 +85,45 @@
          [:div
           (row-hint rh max-row-hints)
           (row r ri)])]]]))
+
+(rum/defc game
+  []
+  (let [{:board-setup/keys [width height probability] :as state} @STATE]
+    (mdl/layout
+     (mdl/header
+      (mdl/header-row
+       (mdl/nav [:h1 "Nonogram"])))
+     (mdl/main-content
+      (mdl/grid
+       (mdl/cell {:mdl [:8]}
+                 (board state))
+       (mdl/cell {:mdl [:4]}
+                 (mdl/card {:mdl [:shadow--2dp]}
+                           (mdl/card-title "Setup")
+                           (mdl/card-action
+                            [:div
+                             [:p
+                              (str "Width (" width ")")
+                              (mdl/slider {:value width
+                                           :min 1
+                                           :max game/max-board-width
+                                           :on-change #(update-setup! :board-setup/width ((comp js/parseInt value) %))})]
+                             [:p
+                              (str "Height (" height ")")
+                              (mdl/slider {:value height
+                                           :min 1
+                                           :max game/max-board-height
+                                           :on-change #(update-setup! :board-setup/height ((comp js/parseInt value) %))})]
+                             [:p
+                              (str "Probability of a block (" (* 100 probability) "%)")
+                              (mdl/slider {:value (* 100 probability)
+                                           :min 0
+                                           :max 100
+                                           :on-change #(update-setup! :board-setup/probability (/ ((comp js/parseInt value) %) 100.0))})]
+                             [:p
+                              (mdl/button {:mdl [:colored :raised :ripple]
+                                           :on-click new-game!}
+                                          "New game")
+                              (mdl/button {:mdl [:colored :raised :ripple]
+                                           :on-click restart-game!}
+                                          "Reset")]]))))))))
--- styles/nonogram/styles.clj
+++ styles/nonogram/styles.clj
@@ -3,42 +3,37 @@
             [garden.units :refer [px em]]))
 
 (defstyles screen
-  (let [base-size (px 16)
-        cell-size (em 2)
-        background-color "#ddd"
+  (let [cell-size (em 2)
         grid-line [[(px 1) :solid "#333"]]]
-    [[:* :body {:font-family "Arial, sans-serif"
-                :font-size base-size
-                :background-color background-color}]
-     [:div
-      [:.hint :.cell {:width cell-size
-                      :height cell-size
-                      :text-align :center
-                      :vertical-align :middle
-                      :line-height cell-size}]
-      [:.col-hint-container {:float :left
-                             :border-bottom grid-line}
-       [:.row-hints {:float :left
-                     :border :none}]
-       [:.col-hints {:float :left
-                     :position :relative
-                     :width cell-size
-                     :border-left grid-line}
-        [:.hint {:border-right grid-line}]]]
-      [:.rows
-       [:.row-hints {:clear :left}
-        [:.hint {:float :left
-                 :position :relative
-                 :border-bottom grid-line}]]
-       [:.row {:border-left grid-line
-               :float :left}
-        [:.cell {:float :left
-                 :position :relative
-                 :border-bottom grid-line
-                 :border-right grid-line}]
-        [:.cell.none {:background-color :inherit}]
-        [:.cell.yes {:background-color "rgb(128,255,128)"}]
-        [:.cell.no {:background-color "rgb(255,128,128)"}]]]]]))
+    [:div
+     [:.hint :.cell {:width cell-size
+                     :height cell-size
+                     :text-align :center
+                     :vertical-align :middle
+                     :line-height cell-size}]
+     [:.col-hint-container {:float :left
+                            :border-bottom grid-line}
+      [:.row-hints {:float :left
+                    :border :none}]
+      [:.col-hints {:float :left
+                    :position :relative
+                    :width cell-size
+                    :border-left grid-line}
+       [:.hint {:border-right grid-line}]]]
+     [:.rows
+      [:.row-hints {:clear :left}
+       [:.hint {:float :left
+                :position :relative
+                :border-bottom grid-line}]]
+      [:.row {:border-left grid-line
+              :float :left}
+       [:.cell {:float :left
+                :position :relative
+                :border-bottom grid-line
+                :border-right grid-line}]
+       [:.cell.none {:background-color :inherit}]
+       [:.cell.yes {:background-color "rgb(128,255,128)"}]
+       [:.cell.no {:background-color "rgb(255,128,128)"}]]]]))
 
 ;; div.hint {
 ;; 	width: 2em;
